5e7ae2b5f8618f0817db6e891074b7363b53fbdf
Ismail Sinan <ismailsinan2003@gmail.com>
2026-04-06 13:26:36 +0400

    resolved error

--- src/constants/data.js
+++ src/constants/data.js
@@ -2,20 +2,29 @@ const skills = [
   {
     title: "<LANGUAGES/>",
     item: [
-      { name: "JavaScript", img: "public/javascript-logo-svgrepo-com.svg" },
-      { name: "Python", img: "public/python-svgrepo-com.svg" },
-      { name: "HTML", img: "public/html-5-svgrepo-com.svg" },
-      { name: "CSS", img: "public/css-3-svgrepo-com.svg" },
+      {
+        name: "JavaScript",
+        img: "public/images/javascript-logo-svgrepo-com.svg",
+      },
+      { name: "Python", img: "public/images/python-svgrepo-com.svg" },
+      { name: "HTML", img: "public/images/html-5-svgrepo-com.svg" },
+      { name: "CSS", img: "public/images/css-3-svgrepo-com.svg" },
     ],
   },
   {
     title: "<FRAMEWORK/LIBRARIES/>",
     item: [
-      { name: "React", img: "public/react-svgrepo-com.svg" },
-      { name: "Tailwind CSS", img: "public/tailwind-svgrepo-com.svg" },
-      { name: "React Router", img: "public/react-router-svgrepo-com.svg" },
-      { name: "Bootstrap", img: "public/bootstrap-4-logo-svgrepo-com.svg" },
-      { name: "ExpressJS", img: "public/express-svgrepo-com.svg" },
+      { name: "React", img: "public/images/react-svgrepo-com.svg" },
+      { name: "Tailwind CSS", img: "public/images/tailwind-svgrepo-com.svg" },
+      {
+        name: "React Router",
+        img: "public/images/react-router-svgrepo-com.svg",
+      },
+      {
+        name: "Bootstrap",
+        img: "public/images/bootstrap-4-logo-svgrepo-com.svg",
+      },
+      { name: "ExpressJS", img: "public/images/express-svgrepo-com.svg" },
     ],
   },
   {
@@ -23,7 +32,7 @@ const skills = [
     item: [
       {
         name: "NodeJS",
-        img: "public/node-svgrepo-com.svg",
+        img: "public/images/node-svgrepo-com.svg",
       },
     ],
   },
@@ -32,11 +41,11 @@ const skills = [
     item: [
       {
         name: "MongoDB",
-        img: "public/mongo-svgrepo-com.svg",
+        img: "public/images/mongo-svgrepo-com.svg",
       },
       {
         name: "PostgreSQL",
-        img: "public/postgresql-svgrepo-com.svg",
+        img: "public/images/postgresql-svgrepo-com.svg",
       },
     ],
   },
